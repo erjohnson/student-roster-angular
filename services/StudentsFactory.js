157aeebf05d28d18aedf89b6b248b86590332ef8
Eric <erjohnson.pdx@gmail.com>
2014-09-16 15:21:35 -0700

    create students factory

---
+++ services/StudentsFactory.js
@@ -0,0 +1,16 @@
+studentRoster.factory('StudentsFactory', function StudentsFactory() {
+  var factory = {};
+  factory.students = [];
+
+  factory.addStudent = function() {
+    factory.students.push({"name": factory.studentName});
+    factory.studentName = null;
+  };
+
+  factory.deleteStudent = function() {
+    var index = factory.students.indexOf(student)
+    factory.students.splice(index, 1);
+  };
+
+  return factory;
+});

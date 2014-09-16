0eeb56181a748c037d63b895e3a01833c080c3be
Eric <erjohnson.pdx@gmail.com>
2014-09-16 15:14:06 -0700

    add sign-in route

--- app.js
+++ app.js
@@ -7,4 +7,9 @@ studentRoster.config(function ($routeProvider) {
         controller: 'StudentsCtrl',
         templateUrl: 'views/Students.html'
     })
+    .when('/sign-in',
+      {
+        controller: 'SignInCtrl',
+        templateUrl: 'views/SignIn.html'
+      })
 });
